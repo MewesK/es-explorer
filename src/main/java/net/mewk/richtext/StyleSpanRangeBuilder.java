7ff7ecc844044c89c2e3fd95553788d2cf21c8b8
Mewes Kochheim <kochheim@develop4edu.de>
2015-07-23 18:52:12 +0200

    Improved style span range implementation including tests

--- src/main/java/net/mewk/richtext/StyleSpanRangeBuilder.java
+++ src/main/java/net/mewk/richtext/StyleSpanRangeBuilder.java
@@ -14,25 +14,12 @@ public class StyleSpanRangeBuilder extends ArrayList<StyleSpanRange> {
         this.sort((o1, o2) -> Integer.compare(o1.getStart(), o2.getStart()));
 
         // Merge/stamp combined list
-        final Stack<StyleSpanRange> results = new Stack<>();
-        for (StyleSpanRange span : this) {
-            if (results.size() == 0) {
-                results.push(span);
-            } else {
-                StyleSpanRange topSpan = results.peek();
-                if (topSpan.overlaps(span)) {
-                    results.pop();
-                    results.addAll(topSpan.stamp(span));
-                } else {
-                    results.push(span);
-                }
-            }
-        }
+
 
         final StyleSpansBuilder<Collection<String>> styleSpansBuilder = new StyleSpansBuilder<>();
 
         StyleSpanRange lastSpan = new StyleSpanRange();
-        for (StyleSpanRange span : results) {
+        for (StyleSpanRange span : merge()) {
             // Create blank span if necessary
             if (lastSpan.getEnd() < span.getStart()) {
                 StyleSpanRange blankSpan = new StyleSpanRange(lastSpan.getEnd(), span.getStart());
@@ -44,5 +31,25 @@ public class StyleSpanRangeBuilder extends ArrayList<StyleSpanRange> {
         }
 
         return styleSpansBuilder.create();
+    }
+
+    private Stack<StyleSpanRange> merge() {
+        final Stack<StyleSpanRange> styleSpanRangeStack = new Stack<>();
+
+        for (StyleSpanRange span : this) {
+            if (styleSpanRangeStack.size() == 0) {
+                styleSpanRangeStack.push(span);
+            } else {
+                StyleSpanRange topSpan = styleSpanRangeStack.peek();
+                if (topSpan.overlaps(span)) {
+                    styleSpanRangeStack.pop();
+                    styleSpanRangeStack.addAll(topSpan.stamp(span));
+                } else {
+                    styleSpanRangeStack.push(span);
+                }
+            }
+        }
+
+        return styleSpanRangeStack;
     }
 }
